eb980a9ad84928213c22ce6eeca652ec524ee5d0
mtfishman <mfishman@flatironinstitute.org>
2024-09-13 22:17:47 -0400

    Fix aqua failures

--- src/lib/NamedIntegers/src/NamedIntegers.jl
+++ src/lib/NamedIntegers/src/NamedIntegers.jl
@@ -9,7 +9,12 @@ unname(::Type{<:NamedInteger{Value}}) where {Value} = Value
 name(ni::NamedInteger) = getfield(ni, :name)
 named(value::Integer, name) = NamedInteger(value, name)
 
-(type::Type{<:Integer})(ni::NamedInteger) = type(unname(ni))
+named_construct_integer(type::Type{<:Integer}, ni::NamedInteger) = type(unname(ni))
+(type::Type{<:Integer})(ni::NamedInteger) = named_construct_integer(type, ni)
+# Fix ambiguity errors.
+Base.Integer(ni::NamedInteger) = named_construct_integer(Integer, ni)
+Base.Bool(ni::NamedInteger) = named_construct_integer(Bool, ni)
+Base.GMP.BigInt(ni::NamedInteger) = named_construct_integer(BigInt, ni)
 
 Base.:*(ni1::NamedInteger, ni2::NamedInteger) = unname(ni1) * unname(ni2)
 function Base.promote_rule(type1::Type{<:NamedInteger}, type2::Type{<:Integer})
